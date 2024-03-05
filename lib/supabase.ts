cdb17168830496fe757bd1d3cb92cbaa612df1c2
Mohamed Irfan <i4irfan777@gmail.com>
2024-03-05 21:01:44 +0530

    add supabase realtime channel connectivity

---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+import 'react-native-url-polyfill/auto'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = "https://qhtlivlvsknownvyopyc.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFodGxpdmx2c2tub3dudnlvcHljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2MDc3OTYsImV4cCI6MjAyNTE4Mzc5Nn0.n-x9gNzDj7_eIsv7Yc98qTRzFd1vz2JyrjrZCYwCcQ8"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
