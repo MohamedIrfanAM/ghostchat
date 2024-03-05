4a41283845537d9a8e074ddeb11f6a4473d98a79
Mohamed Irfan <i4irfan777@gmail.com>
2024-03-05 21:02:14 +0530

    supbase realtime subscribe and recieve functionality

--- app/chat/index.tsx
+++ app/chat/index.tsx
@@ -1,25 +1,54 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import { supabase } from '@/lib/supabase';
+import React, { useState, useCallback, useEffect, useRef } from 'react'
 import { GiftedChat, IMessage } from 'react-native-gifted-chat'
+
 
 const index = () => {
   const [messages, setMessages] = useState<IMessage[]>([])
+  const messageId = useRef(0)
+
+  const sendMessage = async (message: string) => {
+    if (!message) return; 
+
+    try {
+      const channel = supabase.channel('room-1'); 
+      await channel.send({
+        type: 'broadcast',
+        event: 'send',
+        payload: { message },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
+    const channel = supabase.channel('room-1'); 
+
+    const messageReceived = (payload:any) => {
+      const newMessage: IMessage = {
+        _id: messageId.current,
+        text: payload?.payload?.message,
         createdAt: new Date(),
         user: {
           _id: 2,
           name: 'React Native',
           avatar: 'https://placeimg.com/140/140/any',
         },
-      },
-    ])
-  }, [])
+      }
+
+      setMessages((prevMessages) => [newMessage,...prevMessages]);
+      messageId.current += 1
+    };
+    channel.on('broadcast', { event: 'recieve' }, messageReceived).subscribe();
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, []);
 
   const onSend = useCallback((messages:IMessage[] = []) => {
+    sendMessage(messages[0].text)
     setMessages(previousMessages =>
       GiftedChat.append(previousMessages, messages),
     )
