8c2d89d36e465ce0b7fa62995e00f5aaa41c4dab
Mohamed Irfan <i4irfan777@gmail.com>
2024-03-06 16:55:33 +0530

    auth email verification

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,29 @@ import { StyleSheet } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
 import { Link } from 'expo-router';
+import 'react-native-url-polyfill/auto'
+import { useState, useEffect } from 'react'
+import { supabase } from '@/lib/supabase';
+import Auth from '@/components/Auth';
+// import Account from './components/Account'
+import { Session } from '@supabase/supabase-js'
 
 export default function TabOneScreen() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
   return (
-    <View style={styles.container}>
+    <View>
+      <Auth />
+      {session && session.user && <Text>{session.user.id}</Text>}
       <Link href={"/chat/"}>
         <Text style={styles.title}>Tab One</Text>
       </Link>
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,3 +1,4 @@
+import { AppState } from 'react-native'
 import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
@@ -12,4 +13,12 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     persistSession: true,
     detectSessionInUrl: false,
   },
+})
+
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh()
+  } else {
+    supabase.auth.stopAutoRefresh()
+  }
 })
