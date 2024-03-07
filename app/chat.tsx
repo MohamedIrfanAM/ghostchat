e4c58b197a7035c9486c20e083f935aba6900427
Mohamed Irfan <i4irfan777@gmail.com>
2024-03-07 20:59:25 +0530

    get guestProfile and send to channel with the username

--- app/chat.tsx
+++ app/chat.tsx
@@ -2,26 +2,56 @@ import { supabase } from '@/lib/supabase';
 import React, { useState, useCallback, useEffect, useRef } from 'react'
 import { GiftedChat, IMessage } from 'react-native-gifted-chat'
 import { useProfileStore } from '@/stores/profileStore'
-import { RealtimeChannel } from '@supabase/supabase-js';
 import { router } from 'expo-router';
 
+type profile = {
+  displayname: string
+  imageurl: string
+}
 
 const index = () => {
   const [messages, setMessages] = useState<IMessage[]>([])
   const messageId = useRef(0)
   const userId = useProfileStore(state => state.id)
-  const profile = useProfileStore(state => state.profile)
+  const [guestId, setGuestId] = useState('')
+  // const profile = useProfileStore(state => state.profile)
   const [roomID, setRoomID] = useState('')
+  const [guestProfile, setGuestProfile] = useState<profile>({ displayname: '', imageurl: '' })
 
-  const deleteQueueEntry = async () => {
+  const deleteQueueEntry = async (id: string) => {
     const { data, error } = await supabase
       .from('queue')
       .delete()
-      .eq('userID', userId)
-    if (error) {
+      .eq('userID', id)
+    if (error !== null) {
+      console.log("error whle deleting queue entry")
       console.log(error)
     }
   }
+
+  const getGuestProfile = async (roomID: number) => {
+    const { data, error } = await supabase
+      .from('queue')
+      .select(`
+        userID,
+        profiles(
+          displayname,
+          imageurl
+        )
+      `)
+      .eq('roomID', roomID)
+      .neq('userID', userId)
+
+    if (error) {
+      console.log(error)
+      return
+    }
+    const profile = data[0].profiles
+    setGuestProfile(profile)
+    setGuestId(data[0].userID)
+    deleteQueueEntry(data[0].userID)
+  }
+
 
   useEffect(() => {
     if (userId == '') {
@@ -39,9 +69,10 @@ const index = () => {
         },
         (payload) => {
           const roomId = payload?.new?.roomID
+          const state = payload?.new?.state
           if (roomId !== null) {
             setRoomID(roomId)
-            deleteQueueEntry()
+            getGuestProfile(parseInt(roomId))
           }
         }
       )
@@ -50,23 +81,25 @@ const index = () => {
 
   useEffect(() => {
     if (roomID !== '') {
-      const channel = supabase.channel('room-' + roomID)
+      const channel = supabase.channel(userId) 
       const messageReceived = (payload: any) => {
+        const message = payload?.payload?.message
         const newMessage: IMessage = {
           _id: messageId.current,
-          text: payload?.payload?.message,
+          text: message,
           createdAt: new Date(),
           user: {
             _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
+            name: guestProfile.displayname,
+            avatar: guestProfile.imageurl,
           },
         }
+
         setMessages((prevMessages) => [newMessage, ...prevMessages]);
         messageId.current += 1
       };
 
-      channel.on('broadcast', { event: 'send' }, messageReceived).subscribe();
+      channel.on('broadcast', { event: 'message' }, messageReceived).subscribe();
 
       return () => {
         channel.unsubscribe();
@@ -75,15 +108,17 @@ const index = () => {
   }, [roomID]);
 
   const sendMessage = async (message: string) => {
+
     if (!message || roomID == '') {
       console.log('no message or roomID')
       return;
+
     }
-    const channel = supabase.channel('room-' + roomID)
+    const channel = supabase.channel(guestId)
     try {
       await channel.send({
         type: 'broadcast',
-        event: 'receive',
+        event: 'message',
         payload: { message },
       });
     } catch (error) {
